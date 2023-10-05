aaee668fe4a68927fb8714efc8fc2ea5c98bc591
LuisBuis22 <luis.buisan@gmail.com>
2023-10-05 10:45:46 +0200

    Arreglos

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// jest.config.js
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+};
+
