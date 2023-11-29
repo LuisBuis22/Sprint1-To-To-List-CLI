1ec02ee90241855ebc36e8fd57b1be769182b5c8
LuisBuis22 <luis.buisan@gmail.com>
2023-11-29 12:47:01 +0100

    Corrección errores mayúsculas

--- src/__tests__/TaskController.test.ts
+++ src/__tests__/TaskController.test.ts
@@ -1,4 +1,4 @@
-import { TaskController } from "../controllers/Taskcontroller";
+import { TaskController } from "../controllers/TaskController";
 
 describe("TaskController", () => {
   let taskController: TaskController;
@@ -32,11 +32,8 @@ describe("TaskController", () => {
       const tasksBeforeDeletion = taskController.getTasks();
       const taskIdToDelete = tasksBeforeDeletion[0].id;
 
-<<<<<<< HEAD
-=======
       taskController.deleteTask(taskIdToDelete);
 
->>>>>>> ce7c01ca4eab681127080892ace8407410d1faca
       const tasksAfterDeletion = taskController.getTasks();
       expect(
         tasksAfterDeletion.find((task: { id: any; }) => task.id === taskIdToDelete)
